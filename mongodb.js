4dc774f87517eefad3fa89f3216d2c28765f4d25
Xing Ern <lxingern@gmail.com>
2023-01-10 14:40:29 +0800

    add documents into mongodb collections

--- mongodb.js
+++ mongodb.js
@@ -9,8 +9,44 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database!')
     }
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Xing Ern',
-        age: 27
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Xing Ern',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+    //     console.log(result.ops)
+    // })
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert users')
+    //     }
+    //     console.log(result.ops)
+    // })
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Clean room',
+    //         completed: false
+    //     }, {
+    //         description: 'Apply for jobs',
+    //         completed: true
+    //     } , {
+    //         description: 'Practise music',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert tasks')
+    //     }
+    //     console.log(result.ops)
+    // })
 })
