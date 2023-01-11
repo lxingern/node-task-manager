06fc95d50b1c60d5b9a7fcc8ada0cc4f0c3d84da
Xing Ern <lxingern@gmail.com>
2023-01-11 16:33:35 +0800

    move User model into its own file in src/models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,60 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid.')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number.')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error("Password should not contain the word 'password'.")
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '  Xing Ern ',
-//     email: 'XINGERN@EMAIL.COM ',
-//     password: 'ajsdfef4322'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -66,15 +14,4 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
-})
-
-const task = new Task({
-    description: 'complete Node.js course',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
 })
