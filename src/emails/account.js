a47694b3a14ad23333d80651e9bc8c6d5ef46e99
Xing Ern <lxingern@gmail.com>
2023-01-15 16:54:00 +0800

    send an email using sendgrid

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,2 +1,12 @@
-const sendgridAPIKey = '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'
 const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+sgMail.send({
+    to: 'lxingern@gmail.com',
+    from: 'lxingern@gmail.com',
+    subject: 'This is my first creation!',
+    text: 'I hope this one actually gets to you.'
+})
